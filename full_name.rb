dbc131fb296d5eddc59454f8c7deea9956df6b07
Kristina Markina <kr.markina@gmail.com>
2021-03-31 19:00:31 -0400

    Update full_name.rb

--- full_name.rb
+++ full_name.rb
@@ -1,3 +1,16 @@
+class FullName
+  attr_accessor :first_name, :middle_name, :last_name
+
+  def initialize(first_name, middle_name, last_name)
+    @full_name = first_name + " "+ middle_name + " " + last_name
+  end
+
+  def to_s
+    puts @full_name
+  end
+  
+end
+
 def ask(question)
   print question + ' '
   gets.chomp
@@ -7,6 +20,6 @@ full_name = Array.new()
 first_name = ask("enter you first name")
 middle_name = ask("enter you middle name")
 last_name = ask("enter you last name")
-full_name.insert(0, first_name, middle_name, last_name)
+full_name = FullName.new(first_name, middle_name, last_name)
 print "Hello, "
-full_name.each do |name| print "#{name} " end
+full_name.to_s
